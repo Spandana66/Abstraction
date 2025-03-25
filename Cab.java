57141d6dbf06c8fb61ef52d371c46656d5211c46
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-25 11:47:50 +0530

    Create Cab.java

---
+++ Cab.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class Cab implements Rapido1
+{
+	public void start_ride()
+	{
+		System.out.println("Bike reached the starting location");
+	}
+	public void continue_ride()
+	{
+		System.out.println("ride continued on Bike");
+	}
+	public void end_ride()
+	{
+		System.out.println("Arraived to destination on Bike");
+	}
+}
