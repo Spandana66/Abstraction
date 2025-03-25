b15d5011575c17e2ec17393251498687f337520c
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-25 11:43:14 +0530

    Create Phonepay.java

---
+++ Phonepay.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class Phonepay extends Hierarchical
+{
+	public void balance()
+	{
+		System.out.println("balance from Gpay");
+	}
+	public void deductmoney()
+	{
+		System.out.println("deducted from Gpay");
+	}
+	public void addmoney()
+	{
+		System.out.println("money added toGpay");
+	}
+}
