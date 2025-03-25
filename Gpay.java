027fd763f1fc041b4024db21235a152f04fef7c5
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-25 11:42:34 +0530

    Create Gpay.java

---
+++ Gpay.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class Gpay extends Hierarchical
+{
+public void balance()
+{
+	System.out.println("balance from Gpay");
+}
+public void deductmoney()
+{
+	System.out.println("deducted from Gpay");
+}
+public void addmoney()
+{
+	System.out.println("money added toGpay");
+}
+}
