7cf4d633be840080d145bf5e953e798eea91306b
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-26 12:50:00 +0530

    Create SBI.java

---
+++ SBI.java
@@ -0,0 +1,13 @@
+package Abstraction;
+
+public class SBI extends Atm
+{
+public void balance()
+{
+	System.out.println("balance from SBI card");
+}
+public void withdraw()
+{
+	System.out.println("money deducted from SBI account");
+}
+}
