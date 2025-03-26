a87af77e5106e6c8efa7150b74c30b43885ff2b7
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-26 12:50:40 +0530

    Create HDFC.java

---
+++ HDFC.java
@@ -0,0 +1,13 @@
+package Abstraction;
+
+public class HDFC extends Atm
+{
+	public void balance()
+	{
+		System.out.println("balance from HDFC card");
+	}
+	public void withdraw()
+	{
+		System.out.println("money deducted from HDFC account");
+	}
+}
