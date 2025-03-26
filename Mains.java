680a5fe82ce648da9c0b3f008b4acdda30df3cc4
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-26 12:53:23 +0530

    Create Mains.java

---
+++ Mains.java
@@ -0,0 +1,12 @@
+package Abstraction;
+
+public class Mains 
+{
+public static void main(String[] args) 
+{
+Demo d=Demo.obj();
+System.out.println(d);
+Demo d1=Demo.obj();
+System.out.println(d1);
+}
+}
